019b7af7774954bf117cf2c4e962d73beae494cd
Julio Mafra <julioclmafra@gmail.com>
2023-06-20 13:14:17 -0300

    adicionando insert

--- src/app/database/activerecord/Insert.php
+++ src/app/database/activerecord/Insert.php
@@ -2,15 +2,35 @@
 
 namespace Activerecord\app\database\activerecord;
 
+use Activerecord\app\database\connection\Connection;
 use Activerecord\app\database\interfaces\ActiveRecordExecuteInterface;
 use Activerecord\app\database\interfaces\ActiveRecordInterface;
 use Activerecord\app\database\interfaces\InsertInterface;
+use Exception;
 
 class Insert implements ActiveRecordExecuteInterface
 {
+
     public function execute(ActiveRecordInterface $activeRecordInterface): void
     {
-        echo "inserindo..";
+        $sql = $this->createQuery($activeRecordInterface);
+
+        try {
+            $pdo = Connection::connect()->prepare($sql);
+            $pdo->execute($activeRecordInterface->getAttributes());
+            echo $pdo->rowCount();
+        } catch (\Throwable $th) {
+            formatExcetion($th);
+        }
     }
 
+    private function createQuery(ActiveRecordInterface $activeRecordInterface): string
+    {
+
+        $sql = "INSERT INTO {$activeRecordInterface->getTable()} (";
+        $sql .= implode(', ', array_keys($activeRecordInterface->getAttributes())) . ")";
+        $sql .= " VALUES (:" . implode(', :', array_keys($activeRecordInterface->getAttributes())) . ")";
+
+        return $sql;
+    }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -7,6 +7,6 @@ use Activerecord\app\database\activerecord\Update;
 use Activerecord\app\database\models\User;
 
 $update = new User;
-$update->firstName = 'Julio';
-$update->lastName = 'Mafra';
-$update->execute(new Update('id', '1'));
+$update->firstName = 'João';
+$update->lastName = 'Marcos';
+$update->execute(new Insert);
